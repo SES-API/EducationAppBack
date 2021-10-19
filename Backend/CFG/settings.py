052dac9b9fc55ba1c6d41435a398437a3310db40
Mahdi <amirmahdishadman98@gmail.com>
2021-10-19 20:12:41 +0330

    Creating a Custom User Model in account app

--- Backend/CFG/settings.py
+++ Backend/CFG/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'rest_framework',
     #swager:
     'drf_yasg',
+    #account:
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +129,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#set drf use which user model: 
+AUTH_USER_MODEL="account.User"
