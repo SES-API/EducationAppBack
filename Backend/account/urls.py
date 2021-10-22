df2e1588f1a5dc958f7577663e9e4f1ef747640b
Mahdi <amirmahdishadman98@gmail.com>
2021-10-22 20:37:16 +0330

    add login view

--- Backend/account/urls.py
+++ Backend/account/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path,include,re_path
 # from rest_framework import routers
 from .views import *
-
+from rest_framework.authtoken.views import obtain_auth_token
 
 app_name='account'
 
@@ -15,5 +15,6 @@ urlpatterns = [
     path('change_password/', ChangePasswordView.as_view(), name='change_password'),
     path('send_register_email/',SendRegisterEmail.as_view(),name='send_register_email'),
     path('send_reset_password_email/',SendResetPasswordEmail.as_view(),name='send_reset_password_email'),
-    path('reset_password/',ResetPasswordView.as_view(),name='reset_password')
+    path('reset_password/',ResetPasswordView.as_view(),name='reset_password'),
+    path('login/', obtain_auth_token, name='login')
 ]
