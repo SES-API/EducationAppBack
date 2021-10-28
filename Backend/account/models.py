6796853b5807788e67e3fdb5dc8333096b074626
shahrzadazari <shahrzad2780@example.com>
2021-10-28 15:01:15 +0330

    model username

--- Backend/account/models.py
+++ Backend/account/models.py
@@ -4,7 +4,6 @@ from django.core.validators import RegexValidator
 # Create your models here.
 
 class User(AbstractUser):
-    username = models.CharField(unique=True, blank=False, max_length=30)
     password = models.CharField(
         blank=False,
         max_length=30,
@@ -13,5 +12,3 @@ class User(AbstractUser):
             RegexValidator(regex="^(?=.{8,})",message='Password must be eight characters or longer.')]
         )
     email = models.EmailField(unique=True, blank=False)
-    # first_name = models.CharField(max_length=30, blank=True)
-    # last_name = models.CharField(max_length=30, blank=True)
