5189e30a39271c1bd034268ed202c85a85cc08bc
shahrzadazari <shahrzad2780@example.com>
2021-11-03 17:26:47 +0330

    update user model

--- Backend/account/models.py
+++ Backend/account/models.py
@@ -4,6 +4,10 @@ from django.core.validators import RegexValidator
 # Create your models here.
 
 class User(AbstractUser):
+    GENDER = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
     password = models.CharField(
         blank=False,
         max_length=30,
@@ -12,3 +16,10 @@ class User(AbstractUser):
             RegexValidator(regex="^(?=.{8,})",message='Password must be eight characters or longer.')]
         )
     email = models.EmailField(unique=True, blank=False)
+    first_name = models.CharField(max_length=30,null=True)
+    last_name = models.CharField(max_length=30,null=True)
+    gender = models.CharField(max_length=1, choices=GENDER, null=True)
+    birthdate = models.DateField(null=True)
+    degree = models.CharField(max_length=30,null=True)
+    university = models.CharField(max_length=50,null=True)
+    profile_pic = models.ImageField(default="default_profile.jpg")
