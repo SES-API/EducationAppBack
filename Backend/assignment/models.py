0d0918b9190c24337a4bc076b91774c6a41f0eb3
shahrzadazari <shahrzad2780@example.com>
2021-11-17 21:23:28 +0330

    question and model class

--- Backend/assignment/models.py
+++ Backend/assignment/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
+from ..class.models import Class
 # Create your models here.
+
+
+UserModel=get_user_model()
+
+class Assignment(models.Model):
+    name = models.CharField(max_length=50)
+    date = models.DateField()
+    delay = models.FloatField() # eg. 40%
+    questions = models.ManyToManyField(Question,related_name="assignment_question", through='grade')
+    class_id = models.ForeignKey(Class,related_name="assignment_class",on_delete=models.CASCADE)
+
+
+
+class Question(models.Model):
+    name = models.CharField(max_length=50)
+    coefficient = models.FloatField() # eg. 40%
+    students = models.ManyToManyField(UserModel,related_name="question_student")
