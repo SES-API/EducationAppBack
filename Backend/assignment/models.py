c235da9fffe20911f7657e4090af779ba453e033
shahrzadazari <shahrzad2780@example.com>
2021-11-19 18:45:26 +0330

    change question model

--- Backend/assignment/models.py
+++ Backend/assignment/models.py
@@ -3,25 +3,28 @@ from django.contrib.auth import get_user_model
 # Create your models here.
 from django.apps import apps
 from class_app.models import Class
+from account.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
+import datetime
 
-
-UserModel=get_user_model()
 
 
 class Question(models.Model):
     name = models.CharField(max_length=50)
-    weight = models.FloatField() # eg. 40%
-    students = models.ManyToManyField(UserModel,related_name="question_student")
+    weight = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(1)]) # eg. 40%
+    students = models.ManyToManyField(User,related_name="question_student", through='Grade')
 
 class Assignment(models.Model):
     name = models.CharField(max_length=50)
-    date = models.DateField()
-    delay = models.FloatField() # eg. 40%
-    questions = models.ManyToManyField(Question,related_name="assignment_question", through='Grade')
-    class_id = models.ForeignKey(Class,related_name="assignment_class",on_delete=models.CASCADE)
+    date = models.DateField(default=datetime.date.today)
+    questions = models.ManyToManyField(Question,related_name="assignment_question")
+    class_fk = models.ForeignKey(Class,related_name="assignment_class",on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('name', 'class_fk',)
 
 class Grade(models.Model):
-    person = models.ForeignKey(Question, on_delete=models.CASCADE)
-    group = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    student = models.ForeignKey(User, on_delete=models.CASCADE)
     value = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+    delay = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(1)]) # eg. 40%
