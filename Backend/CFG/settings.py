d328d2a1f4609dd226aa5860a4a9ba8b059566da
Mahdi <amirmahdishadman98@gmail.com>
2021-11-25 21:28:56 +0330

    fix a small bug

--- Backend/CFG/settings.py
+++ Backend/CFG/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_filters',
+    # 'django_filters',
     #drf:
     'rest_framework',
     #auth token
