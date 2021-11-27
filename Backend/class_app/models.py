250cdd34a442484b0c7cd66a4cff8c2bbe5f0591
shahrzadazari <shahrzad2780@example.com>
2021-11-27 20:25:54 +0330

    unique constraint on university model

--- Backend/class_app/models.py
+++ Backend/class_app/models.py
@@ -19,7 +19,7 @@ class Semester(models.Model):
 
 
 class University(models.Model):
-    name=models.CharField(max_length=50)
+    name=models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
