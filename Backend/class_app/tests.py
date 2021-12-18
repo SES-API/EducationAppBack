21713b7c226253dcae6292559833e1045e62e3c4
Mahdi <amirmahdishadman98@gmail.com>
2021-12-18 21:31:23 +0330

    write 2 test for class_app

--- Backend/class_app/tests.py
+++ Backend/class_app/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
-
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIClient
+from django.urls import reverse
+from rest_framework import status
 # Create your tests here.
+factory = APIRequestFactory()
+client = APIClient()
+
+
+
+
+from .models import Class
+from account.models import User
+
+
+
+class ClassTests(APITestCase):
+
+    def setUp(self):
+        User.objects.create(
+            username = "unittestusername",
+            password = "unittestpassword",
+            email= "unittestemail@mail.com",
+        )
+        
+
+    def testcreateclass(self):
+        # self.client.login(username='unittestusername', password='unittestpassword')
+        # self.token = Token.objects.get(user__username='unittestusername')
+        # self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        user = User.objects.get(username='unittestusername')
+        self.client.force_authenticate(user=user)
+        url = reverse('class:classes')
+        data = {'name': 'unitclasstest'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def testgetclass(self):
+        url = reverse('class:classes')
+        url+="?name=unittestusername"
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
