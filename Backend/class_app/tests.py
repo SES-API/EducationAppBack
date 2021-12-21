a6ee2275af8d21464629fa38875d541d92a3504c
Mahdi <amirmahdishadman98@gmail.com>
2021-12-21 16:18:19 +0330

    add and edit test of class app

--- Backend/class_app/tests.py
+++ Backend/class_app/tests.py
@@ -1,3 +1,4 @@
+from typing import overload
 from django.test import TestCase
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
@@ -21,25 +22,58 @@ class ClassTests(APITestCase):
 
     def setUp(self):
         User.objects.create(
-            username = "unittestusername",
+            username = "unittestusername1",
             password = "unittestpassword",
-            email= "unittestemail@mail.com",
+            email= "unittestemail1@mail.com",
         )
-        
+        User.objects.create(
+            username = "unittestusername2",
+            password = "unittestpassword",
+            email= "unittestemail2@mail.com",
+        )
 
-    def testcreateclass(self):
+
+
+        user = User.objects.get(username='unittestusername1')
+        class_=Class.objects.create(
+            name="unitclasstest",
+            description="initialized class",
+            owner=user,
+        )
+        class_.teachers.add(user)
+    
+    def test_createclass(self):
         # self.client.login(username='unittestusername', password='unittestpassword')
         # self.token = Token.objects.get(user__username='unittestusername')
         # self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
-        user = User.objects.get(username='unittestusername')
+        user = User.objects.get(username='unittestusername1')
         self.client.force_authenticate(user=user)
         url = reverse('class:classes')
-        data = {'name': 'unitclasstest'}
+        data = {'name': 'unitclasstest2'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Class.objects.count(), 2)
+        self.assertEqual(Class.objects.get(name='unitclasstest2').name, 'unitclasstest2')
 
-    def testgetclass(self):
+    def test_getclass_with_name(self):
         url = reverse('class:classes')
-        url+="?name=unittestusername"
+        url+="?name=unitclasstest"
         response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_getclass(self):
+        url = reverse('class:classes')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_unique_class_wtih_id(self):
+        # self.client.login(username='unittestusername', password='unittestpassword')
+        # self.token = Token.objects.get(user__username='unittestusername')
+        # self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        user = User.objects.get(username='unittestusername1')
+        class_pk=Class.objects.get(name="unitclasstest").pk
+        self.client.force_authenticate(user=user)
+        url = reverse('class:class_object', kwargs={'pk': class_pk})
+        response = self.client.get(url, format='json')
+        print(response.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
