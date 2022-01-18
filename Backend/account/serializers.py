15ba85313ad1d14c5aa32f944326d4a058e642db
Mahdi <amirmahdishadman98@gmail.com>
2022-01-18 12:47:49 +0330

    add first and last name to register

--- Backend/account/serializers.py
+++ Backend/account/serializers.py
@@ -22,19 +22,25 @@ class UserSerializer(serializers.ModelSerializer):
             'username',
             'password',
             'email',
+            'first_name',
+            'last_name'
         )
         extra_kwargs = {
             'password' : {'write_only':True},
             'id' : {'read_only':True},
             'username' : {'required':True},
-            'email' : {'required':True}
+            'email' : {'required':True},
+            'first_name' : {'required':True},
+            'last_name' : {'required':True}
         }
 
     def create(self, validated_data):
         user = User_Model.objects.create_user(
             username = validated_data['username'],
             email = validated_data['email'],
-            password = validated_data['password']
+            password = validated_data['password'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
         )
         return user
 
